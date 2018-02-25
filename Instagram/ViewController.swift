927c5fddc17cf20b409aa5f79b8a3da37976ac77
TiAuna Dodd <tiaunadodd@TiAunas-MacBook-Pro.local>
2018-02-24 19:48:58 -0500

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by TiAuna Dodd on 2/24/18.
+//  Copyright © 2018 TiAuna Dodd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
